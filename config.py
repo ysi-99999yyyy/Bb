0499d57933b0ff543910aec2a742ffc7152c261a
ysi-99999yyyy <francjulian2@gmail.com>
2024-06-16 04:45:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/PYYRQ/BOT-NY",
+    "https://github.com/ysi-99999yyyy/Bb",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
